5695dfb9fbde906c8937a47057c90a3f87d10bf5
Americo Sierra <eltrogloditakia@gmail.com>
2020-11-08 00:59:42 -0500

    topics

---
+++ 2. Java Object-Oriented Approach/constructors/example1/Chick.java
@@ -0,0 +1,9 @@
+public class Chick {
+
+    public Chick() {
+        System.out.println("in constructor");
+    }
+
+    public void Chick() {} //NOT A CONSTRUCTOR
+
+}
---
+++ 2. Java Object-Oriented Approach/constructors/example1/Chicken.java
@@ -0,0 +1,13 @@
+public class Chicken {
+    int numEggs = 12; //initialize on line
+
+    String name;
+
+    /*public Chicken() {
+        name = "Duke"; //initialize in constructor
+    }*/
+
+    public static void main(String[] args) {
+        Chicken chicken = new Chicken(); //constructor default
+    }
+}
